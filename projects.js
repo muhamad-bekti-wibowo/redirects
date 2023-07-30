b49a46d6438ea64002d86152df831763e3b6812f
muhamad-bekti-wibowo <116127216+muhamad-bekti-wibowo@users.noreply.github.com>
2023-07-30 22:12:57 +0700

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
     console.log(window.location.href);
-    if (window.location.href != "https://muhamad-bekti-wibowo.blogspot.com/") {
+    if (window.location.href = "https://muhamad-bekti-wibowo.blogspot.com/") {
         document.getElementById("NavLink").style.height = "0%";
     } else {
+        $("body").css("display", "none");
         $.get("https://api.onedrive.com/v1.0/shares/s!AuOxR9BjfloLg5Ndgy8eqXEPchy4YQ/root/content", function (data, status) {
             document.getElementById("Projects").href = data;
         });
+        
     }
 });
 function closeNav() {
+    $("body").css("display", "block");
     document.getElementById("NavLink").style.height = "0%";
 }
